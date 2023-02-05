485a8f2c19d8573f74804477628ed24da943eef2
Justin Mari Maliwat <justinmarimari@gmail.com>
2023-02-04 20:37:28 -0500

    Added error handler when fetching projects from the backend

--- src/helpers/projectsHelper.js
+++ src/helpers/projectsHelper.js
@@ -3,6 +3,9 @@ import axiosService from "../services/axiosService";
 function getProjects() {
   return axiosService.get(`/api/projects`).then(result => {
     return result.data;
+  }).catch(error => {
+    console.error("Error when fetching projects from backend.", error)
+    return [];
   });
 }
 
